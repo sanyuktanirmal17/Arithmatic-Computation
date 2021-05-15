0bfecfd3a5122f84a725a5dd43c9fa1230cd0777
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-05-15 19:32:19 +0530

    Added : take input5

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
+declare  -A  comput
 echo "Enter the three inputs :: "
 read a b c
 
-result=$(($a%$b+$c))
-echo $result
+result1=$(($a*$b+$c))
+result2=$(($a+$b*$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
 
+compute[result1]=$result1
+compute[result2]=$result2
+compute[result3]=$result3
+compute[result4]=$result4
+
+echo  All results : ${!compute[@]}
