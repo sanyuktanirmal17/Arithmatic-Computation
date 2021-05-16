551667334cbb61d77013fa556639ad6a398b06d1
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-05-16 22:02:05 +0530

    Added: Ascending and desending

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-declare  -A  comput
+declare  -A  Array
 echo "Enter the three inputs :: "
 read a b c
 
@@ -9,11 +9,50 @@ result2=$(($a+$b*$c))
 result3=$(($c+$a/$b))
 result4=$(($a%$b+$c))
 
-compute[result1]=$result1
-compute[result2]=$result2
-compute[result3]=$result3
-compute[result4]=$result4
+Array[result1]=$result1
+Array[result2]=$result2
+Array[result3]=$result3
+Array[result4]=$result4
 
 echo descending order : ${!compute[@]}
+
+
+for (( i = 0; i <= 3; i++ ))
+do
+   for (( j = $i; j <= 3; j++ ))
+   do
+      if [ ${Array[$i]} -gt ${Array[$j]}  ]; then
+       t=${Array[$i]}
+       Array[$i]=${Array[$j]}
+       Array[$j]=$t
+      fi
+   done
+done
+
+echo -e "\nSorted Numbers in ascending Order:"
+for (( i=0; i <= 3; i++ )) 
+do
+  echo ${Array[$i]}
+done
+for (( i = 0; i <= 3; i++ ))
+do
+   for (( j = $i; j <= 3; j++ ))
+   do
+      if [ ${Array[$i]} -lt ${Array[$j]}  ]; then
+       t=${Array[$i]}
+       Array[$i]=${Array[$j]}
+       Array[$j]=$t
+      fi
+   done
+done
+
+echo -e "\nSorted Numbers in Descending Order:"
+for (( i=0; i <= 3; i++ ))
+do
+  echo ${Array[$i]}
+
+done
+
+
 
 
